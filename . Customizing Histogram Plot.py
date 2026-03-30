6f334ecbc5c2aef8bee7aa81a891e3344996884f
LIKHITH  HP <hplikhith690@gmail.com>
2026-03-30 20:21:44 +0530

    Add files via upload

---
+++ . Customizing Histogram Plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [7, 8, 9, 10, 10, 12, 12, 12, 13, 14, 14, 15, 16, 16, 17,
+     18, 18, 19, 20, 20, 21, 22, 23, 24, 25, 25, 26, 28, 30,
+     32, 35, 36, 38, 40, 42, 44, 48, 50]
+
+plt.hist(x, bins=10, color='green', edgecolor='blue',linestyle='--', alpha=0.5)
+
+plt.title("Customizing Histogram Plot")
+plt.xlabel("Total Bill")
+plt.ylabel("Frequency")
+plt.show()
